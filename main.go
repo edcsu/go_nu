93c81582d2685bf41f9a67178ee554f9392ee42f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-05-04 01:28:38 +0300

    feat: example of maps

--- main.go
+++ main.go
@@ -20,4 +20,35 @@ func main() {
 
 	fn1, sn1 := getInitials("jane doe")
 	fmt.Println(fn1, sn1)
+
+	menu := map[string]float64{
+		"coffee":  5000.99,
+		"tea":     2500.99,
+		"samosa":  500.99,
+		"colesaw": 2000.99,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["samosa"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// int as key type
+
+	phonebook := map[int]string{
+		256772123456: "Jane",
+		256702123456: "Pendo",
+		256712123456: "Theron",
+		256773123456: "Fonda",
+		256774123456: "Alyson",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[256773123456])
+
+	phonebook[256773123456] = "Anita"
+	fmt.Println(phonebook)
+
 }
